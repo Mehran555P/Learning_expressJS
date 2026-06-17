5548bc1960cd3e46281ea83bcfd781c122954ca8
Mehran Hakimnia <hakimniamehran@gmail.com>
2026-06-17 23:20:47 +0330

    first commit of advanced routing project

---
+++ Advanced_Routing/README.md
@@ -0,0 +1,16 @@
+# Simple Router project
+this project is a very simple routing program with **Express**.
+
+## Program structure:
+
+this program has tree importanrt part:
+
++ Home page route
++ Users page rpute
++ Posts page route
+
+## More about this program:
+
+this program doesn't have any view. Just some basic routing for understanding.
+
+## Thanks for your attention.
---
+++ Advanced_Routing/routes/users.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const router = express.Router()
+
+
+router.get('/', (req, res) => {
+    res.render('users', {
+        title: "Users page",
+        message: "به پروژه اکسپرس و EJS خوش آمدید!",
+        features: ["سادگی کدها", "ساختار هدر و فوتر منظم", "بدون پیچیدگی"]
+    });
+});
+
+
+router.get('/new', (req, res) => {
+    console.log("Create new user")
+    res.send("New user")
+})
+
+router.post('/', (req, res) => {
+    res.send("Create User")
+})
+
+
+router.get('/:id', (req, res) => {
+        res.send(`Get user with ID ${req.params.id}`)
+    })
+
+router.put('/:id', (req, res) => {
+    res.send(`Update user with ID ${req.params.id}`)
+})
+
+router.delete('/:id', (req, res) => {
+    res.send(`Delete user with ID ${req.params.id}`)
+})
+
+module.exports = router
