dd0185a9390565a398e8f5440512d82f19adc384
Mehran Hakimnia <hakimniamehran@gmail.com>
2026-07-04 02:39:58 +0330

    Add email validation

--- Express_Advanced/src/users/usersController.ts
+++ Express_Advanced/src/users/usersController.ts
@@ -20,15 +20,29 @@ router.get('/:id', (req: Request, res: Response) => {
     res.send("get user");
 });
 
-router.post('/', authMiddleware, (req: Request, res: Response) => {
+router.post('/', (req: Request, res: Response) => {
+    const body = req.body;
+    // console.log(body);
+ 
+    if (!body.name || !body.email ) {
+        res.status(400).send({ message: "name or email is required"})
+    }
+
+    // email validation
+    const emailRegex = /^\S+@\S+\.\S+$/;
+    if (!emailRegex.test(body.email)) {
+        res.status(400).send({ message: "email is not valid" });
+    }
+
+
     res.send("create user");
 });
 
-router.put('/:id', authMiddleware, (req: Request, res: Response) => {
+router.put('/:id', (req: Request, res: Response) => {
     res.send("update user");
 });
 
-router.delete('/:id', authMiddleware, (req: Request, res: Response) => {
+router.delete('/:id', (req: Request, res: Response) => {
     res.send("delete user");
 });
 
