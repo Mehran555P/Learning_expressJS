f4bab4edd0024385f01ef1bc6e79910ef4737097
Mehran Hakimnia <hakimniamehran@gmail.com>
2026-07-09 20:14:01 +0330

    Add comment to crud operation

--- Express_Advanced/src/users/usersServices.ts
+++ Express_Advanced/src/users/usersServices.ts
@@ -2,6 +2,8 @@ import type { Request, Response, NextFunction } from "express";
 import type User from "./dtos/userDto.js";
 import usersModel from "../models/usersModel.js";
 
+
+// get all users
 export const getAllUsers = () => {
         return new Promise((resolve, reject) => {
             usersModel.find().then((users) => {
@@ -12,36 +14,8 @@ export const getAllUsers = () => {
         });
 }
 
-export const getOneUser = (id: string) => {
-        return new Promise((resolve, reject) => {
-            usersModel.findById(id).then((user) => {
-                resolve(user);
-            }).catch((err) => {
-                reject(err);
-            });
-        });
-}
 
-export const deleteOneUser = (id: string) => {
-        return new Promise((resolve, reject) => {
-            usersModel.findByIdAndDelete(id).then(() => {
-                resolve("User deleted");
-            }).catch((err) => {
-                reject(err);
-            });
-        });
-}
-
-export const updateOneUser = (id: string, params: User) => {
-        return new Promise((resolve, reject) => {
-            usersModel.findByIdAndUpdate(id, params).then((user) => {
-                resolve(user);
-            }).catch((err) => {
-                reject(err);
-            });
-        });
-}
-
+// create a new user
 export const createNewUser = (user: User) => {
     return new Promise((resolve, reject) => {
         usersModel.create(user).then((user) => {
@@ -52,3 +26,41 @@ export const createNewUser = (user: User) => {
     });
      
 }
+
+
+// get one user
+export const getOneUser = (id: string) => {
+        return new Promise((resolve, reject) => {
+            usersModel.findById(id).then((user) => {
+                resolve(user);
+            }).catch((err) => {
+                reject(err);
+            });
+        });
+}
+
+
+// delete one user
+export const deleteOneUser = (id: string) => {
+        return new Promise((resolve, reject) => {
+            usersModel.findByIdAndDelete(id).then(() => {
+                resolve("User deleted");
+            }).catch((err) => {
+                reject(err);
+            });
+        });
+}
+
+
+// update one user
+export const updateOneUser = (id: string, params: User) => {
+        return new Promise((resolve, reject) => {
+            usersModel.findByIdAndUpdate(id, params).then((user) => {
+                resolve(user);
+            }).catch((err) => {
+                reject(err);
+            });
+        });
+}
+
+
