db03bd1c4b539e05bc7a4f75239413f6ec299cb7
Mehran Hakimnia <hakimniamehran@gmail.com>
2026-07-21 16:36:56 +0330

    Add some routes

--- typeScript/server.js
+++ typeScript/server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+    console.log("home page log");
+    res.send("home page");
+})
 
+app.get('/users', (req, res) => {
+    console.log("users page log");
+    res.send("users page");
+})
 
 app.listen(3000);
