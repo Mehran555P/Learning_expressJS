30de203daf71d2a22dbebe3ee0326a697733f93a
Mehran Hakimnia <hakimniamehran@gmail.com>
2026-08-16 01:29:39 +0330

    Add crud to products

--- authentication/src/products/productsController.ts
+++ authentication/src/products/productsController.ts
@@ -3,19 +3,17 @@ import type { Request, Response, NextFunction } from 'express';
 import ValidationMiddleware from '../middlewares/validateMiddleware.js';
 import CreateProductDto from './dtos/productsCreateDto.js';
 import type Product from './dtos/productDto.js';
-import { createNewProduct } from './productsServices.js';
+import { createNewProduct, deleteOneProduct, getAllProducts, getOneProduct, updateOneProduct } from './productsServices.js';
 const router = express.Router();
 
 // get all products
-router.get('/', (req: Request, res: Response) => {
-    res.send("all products");
-    console.log("all products page")
-});
-
-// get one product
-router.get('/:id', (req: Request, res: Response) => {
-    res.send("one product");
-    console.log("one product page")
+router.get('/', async (req: Request, res: Response) => {
+    try {
+        res.send(await getAllProducts());
+    }catch(err: any) {
+        console.log("error: ", err.message)
+        res.status(500).send({ message: err.message});
+    }
 });
 
 // create a new product
@@ -30,14 +28,40 @@ router.post('/', ValidationMiddleware(CreateProductDto), async(req: Request, res
     }
 });
 
+
+// get one product
+router.get('/:id', async(req: Request, res: Response) => {
+    try {
+        const id = req.params.id as string;
+        res.send(await getOneProduct(id));
+    }catch(err: any) {
+        console.log("error: ", err.message);
+        res.status(500).send({ message: err.message});
+    }
+});
+
+
 // update a product
-router.put('/:id', (req: Request, res: Response) => {
-    // ...
+router.put('/:id', async(req: Request, res: Response) => {
+    try {
+        const id = req.params.id as string;
+        const body: Product = req.body;
+        res.send(await updateOneProduct(id, body));
+    }catch(err: any) {
+        console.log("error: ", err.message)
+        res.status(500).send({ message: err.message});
+    }
 });
 
 // delete a product
-router.delete('/:id', (req: Request, res: Response) => {
-    // ...
+router.delete('/:id', async(req: Request, res: Response) => {
+    try {
+        const id = req.params.id as string;
+        res.send(await deleteOneProduct(id));
+    }catch(err: any) {
+        console.log("error: ", err.message)
+        res.status(500).send({ message: err.message});
+    }
 });
 
 export default router;
--- authentication/src/products/productsServices.ts
+++ authentication/src/products/productsServices.ts
@@ -1,7 +1,19 @@
 import type { Request, Response, NextFunction } from "express";
 import type Product from "./dtos/productDto.js";
 import productModel from "../models/productModel.js";
+import { resolve } from "node:dns";
 
+
+// get all products
+export const getAllProducts = () => {
+    return new Promise((resolve, reject) => {
+        productModel.find().then((products) => {
+            resolve(products);
+        }).catch((err) => {
+            reject(err);
+        });
+    });
+}
 
 // create new product
 export const createNewProduct = (product: Product) => {
@@ -11,4 +23,39 @@ export const createNewProduct = (product: Product) => {
         }).catch((err) => {
             reject(err);
         });
-    })};
+    });
+}
+
+
+// get one product
+export const getOneProduct = (id: string) => {
+    return new Promise((resolve, reject) => {
+        productModel.findById(id).then((product) => {
+            resolve(product);
+        }).catch((err) => {
+            reject(err);
+        });
+    });
+}
+
+// update one product
+export const updateOneProduct = (id: string, product: Product) => {
+    return new Promise((resolve, reject) => {
+        productModel.findByIdAndUpdate(id, product).then((product) => {
+            resolve(product);
+        }).catch((err) => {
+            reject(err);
+        });
+    });
+}
+
+// delete one product
+export const deleteOneProduct = (id: string) => {
+    return new Promise((resolve, reject) => {
+        productModel.findByIdAndDelete(id).then((product) => {
+            resolve(product);
+        }).catch((err) => {
+            reject(err);
+        });
+    });
+}
