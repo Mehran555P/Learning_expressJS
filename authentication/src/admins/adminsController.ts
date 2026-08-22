dd7ce8378fdb5d49b86a775fdd4313288f16bb89
Mehran Hakimnia <hakimniamehran@gmail.com>
2026-08-22 22:00:56 +0330

    add model to admins page

--- authentication/src/admins/adminsController.ts
+++ authentication/src/admins/adminsController.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import type { Request, Response, NextFunction } from "express";
+import ValidationMiddleware from "../middlewares/validateMiddleware.js";
+
+import CreateAdminDto from "./dtos/adminCreateDto.js";
 
 
 const router = express.Router();
@@ -10,6 +13,28 @@ router.get('/', (req: Request, res: Response) => {
     res.send("admins");
 });
 
+// create a new admin
+router.post('/', ValidationMiddleware(CreateAdminDto), (req: Request, res: Response) => {
+    console.log("create a new admin");
+    res.send("create a new admin");
+});
 
+// get a specific admin
+router.get('/:id', (req: Request, res: Response) => {
+    console.log("get a specific admin");
+    res.send("get a specific admin");
+});
+
+// update an existing admin
+router.put('/:id', (req: Request, res: Response) => {
+    console.log("update an existing admin");
+    res.send("update an existing admin");
+});
+
+// delete an existing admin
+router.delete('/:id', (req: Request, res: Response) => {
+    console.log("delete an existing admin");
+    res.send("delete an existing admin");
+});
 
 export default router;
--- authentication/src/admins/adminsServices.ts
+++ authentication/src/admins/adminsServices.ts
@@ -1,1 +1,12 @@
 import type { Request, Response, NextFunction } from "express";
+
+
+
+export const getAllAdmins = (req: Request, res: Response, next: NextFunction) => {
+    console.log("get all admins");
+    res.send("admins");
+}
+
+export const createAdmin = (req: Request, res: Response, next: NextFunction) => {
+    
+}
